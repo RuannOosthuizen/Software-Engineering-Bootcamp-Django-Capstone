1514790eced985f9809a639c654548696468ca3a
RuannOosthuizen <pr0h3ctic@gmail.com>
2023-05-18 10:21:22 +0200

    Added dosctrings to apps in user_auth

--- user_auth/apps.py
+++ user_auth/apps.py
@@ -2,5 +2,19 @@ from django.apps import AppConfig
 
 
 class UserAuthConfig(AppConfig):
+    """
+    Configuration class for the 'user_auth' Django application.
+
+    Attributes:
+        default_auto_field (str): The default auto-generated field type for models.
+        name (str): The name of the application.
+
+    Example usage:
+        >>> config = UserAuthConfig()
+        >>> config.default_auto_field
+        'django.db.models.BigAutoField'
+        >>> config.name
+        'user_auth'
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user_auth'
