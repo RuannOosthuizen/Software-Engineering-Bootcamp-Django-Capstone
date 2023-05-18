2abe92d37bbef1edb222bce210e0f76fb6ddce87
RuannOosthuizen <pr0h3ctic@gmail.com>
2023-05-18 10:15:34 +0200

    Added docstrings to apps in polls

--- polls/apps.py
+++ polls/apps.py
@@ -2,5 +2,19 @@ from django.apps import AppConfig
 
 
 class PollsConfig(AppConfig):
+    """
+    Configuration class for the 'polls' Django application.
+
+    Attributes:
+        default_auto_field (str): The default auto-generated field type for models.
+        name (str): The name of the application.
+
+    Example usage:
+        >>> config = PollsConfig()
+        >>> config.default_auto_field
+        'django.db.models.BigAutoField'
+        >>> config.name
+        'polls'
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'polls'
