1b7459279ba20595f72235542c0710472ba2b86f
RuannOosthuizen <pr0h3ctic@gmail.com>
2023-05-18 10:18:48 +0200

    Added docstrings to apps in homepage

--- homepage/apps.py
+++ homepage/apps.py
@@ -2,5 +2,19 @@ from django.apps import AppConfig
 
 
 class HomepageConfig(AppConfig):
+    """
+    Configuration class for the 'homepage' Django application.
+
+    Attributes:
+        default_auto_field (str): The default auto-generated field type for models.
+        name (str): The name of the application.
+
+    Example usage:
+        >>> config = HomepageConfig()
+        >>> config.default_auto_field
+        'django.db.models.BigAutoField'
+        >>> config.name
+        'homepage'
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'homepage'
